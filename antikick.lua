c5dba0dd8b5442fcf04ac63232328a9e803c8f89
sstvskids <126665568+sstvskids@users.noreply.github.com>
2025-03-20 19:51:30 +1100

    Create antikick.lua

---
+++ antikick.lua
@@ -0,0 +1,13 @@
+local cloneref = cloneref or function(o) return o end
+local lplr: lplr = cloneref(game:GetService('Players')).LocalPlayer
+local VirtualUser: VirtualUser = cloneref(game:GetService('VirtualUser'))
+local AFKCheck
+
+AFKCheck = plr.Idled:Connect(function()
+  VirtualUser:CaptureController()
+  VirtualUser:ClickButton2(Vector2.new())
+end)
+
+getgenv().stopafkservice = function()
+  return AFKCheck:Disconnect()
+end
